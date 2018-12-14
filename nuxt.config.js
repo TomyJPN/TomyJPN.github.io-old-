9daa6a48ad2adaf955b5dad6357e4804bc7d3603
TomyJPN <masaya120331@gmail.com>
2018-12-14 23:15:13 +0900

    プロジェクトを新規作成，ソース移動して復元

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'tomyport',
+    title: 'port3',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,53 @@
 <template>
   <div class="main">
     <my-header></my-header>
-  <section class="container">
-    <router-view></router-view>
-    Tomy's Portfolio Site
-    <!--<div>
-      <app-logo/>
-      <h1 class="title">
-        tomyport
-      </h1>
-      <h2 class="subtitle">
-        tomy's portfolio site.(Under construction)
-              </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>-->
-  </section>
+    <section class="container">
+      <router-view></router-view>
+      Tomy's Portfolio Site
+      <!--<div>
+        <app-logo/>
+        <h1 class="title">
+          tomyport
+        </h1>
+        <h2 class="subtitle">
+          tomy's portfolio site.(Under construction)
+                </h2>
+        <div class="links">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            class="button--green">Documentation</a>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="button--grey">GitHub</a>
+        </div>
+      </div>-->
+    </section>
     <my-footer></my-footer>
   </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-import MyHeader from '~/components/header.vue'
-import MyFooter from '~/components/footer.vue'
-import Profile from '~/components/profile.vue'
+  import AppLogo from '~/components/AppLogo.vue'
+  import MyHeader from '~/components/header.vue'
+  import MyFooter from '~/components/footer.vue'
+  import Profile from '~/components/profile.vue'
 
-const router=new VueRouter({
-  routers // `routes: routes` の短縮表記
-})
+  /*const router=new VueRouter({
+    routers // `routes: routes` の短縮表記
+  })*/
 
-export default {
-  components: {
-    AppLogo,
-    MyHeader,
-    MyFooter
-  },
-  routes:[
-    {path:'/profile',component:Profile}
+  export default {
+    components: {
+      AppLogo,
+      MyHeader,
+      MyFooter
+    },
+    routes:[
+      {path:'/profile',component:Profile}
     ]
-}
+  }
 </script>
 
 <style>
@@ -57,36 +57,36 @@ export default {
     min-height: 100%;
   }
 
-.container {
-  min-height: 90vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  background-color: darkslategray;
-  font-size: 50pt;
-  color: whitesmoke;
-}
+  .container {
+    min-height: 90vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-color: darkslategray;
+    font-size: 50pt;
+    color: whitesmoke;
+  }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .title {
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .links {
+    padding-top: 15px;
+  }
 </style>
 
