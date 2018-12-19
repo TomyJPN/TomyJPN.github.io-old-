6d1949aece7d8e0a4be701fce591adb5581081cf
TomyJPN <masaya120331@gmail.com>
2018-12-20 00:14:39 +0900

    worksの内容を書き込んだ

--- components/profile.vue
+++ components/profile.vue
@@ -2,14 +2,17 @@
   <div class="main">
     <div class="about">
       <h2>Toyota Masaya (a.k.a Tomy,Томиников)</h2>
-      <p>北九州工業高等専門学校(National Institute of Technology,Kitakyushu) <br>情報システムコース 4年</p>
+      <p>北九州工業高等専門学校　(National Institute of Technology, Kitakyushu) <br>情報システムコース 4年</p>
       <p>コンピュータ研究部 H30年度部長</p>
       <h2>Like</h2>
       <p>ゲーム開発，創作，ミリタリー，サバゲー，にじさんじ</p>
       <h2>Language</h2>
-      <p>C, C++, C#, JavaScript, HTML/CSS</p>
+      <p>できる：C, C++, C#, HTML/CSS<br>
+        すこし：JavaScript<br>
+        ふれた：Java, Kotlin, Python
+      </p>
       <h2>Keyword</h2>
-      <p>Unity, DXlib, 3D Modeling, Web, front-end, procon28競技</p>
+      <p>Unity, DXlib, 3D Modeling (blender), Web, front-end, procon28競技</p>
     </div>
   </div>
 </template>
--- components/works.vue
+++ components/works.vue
@@ -1,6 +1,44 @@
 <template>
   <div class="main">
-    work
+    <h2>Kosen危機</h2>
+    <p>3年の高専祭にて展示．3Dモデリングした学校を舞台に，Wiiリモコンと専用フットペダルで敵を倒していくシューティングゲーム．</p>
+    <p>工夫：子どもでも遊べるようにシンプルで直感的な操作，UIを作成<br>
+      実績：H29高専祭クラス展示金賞<br>
+    </p>
+    <p>関連リンク：<a
+      href="https://www.slideshare.net/secret/80HUeiuW1iDBX7?fbclid=IwAR2pv81sARrh_DeRv3-DvViySNUqKSLrxON5Q8oJTVJul3U7u6-EKd8wFQs"
+      target="_blank">企画書(SlideShare)</a></p>
+
+    <hr>
+
+    <h2>Kosen Race</h2>
+    <p>4年の高専祭にて展示．前作から発展して高専全敷地をマップに．Wiiリモコンと専用フットペダルでアイテム(単位)を回収するレースゲーム．</p>
+    <p>工夫：Wiiリモコンの加速度センサからハンドル操作を実装．<br>
+      実績：H30高専祭クラス展示金賞<br>
+    </p>
+    <p>関連リンク：<a
+      href="https://tomy.hatenablog.jp/entry/2018/11/08/222218"
+      target="_blank">制作記(はてなブログ)</a></p>
+
+    <hr>
+
+    <h2>関門G&G (Game & Guide)</h2>
+    <p>市内文系大学の学生と文理合同プロジェクトを立ち上げ開発した，関門地域活性化を目指す観光支援アプリ<br>
+      回遊性の向上や売上の向上を目指すほか，観光客から観光データを収集し，情報を提供することで商売や観光促進に使っていただくことを狙っています.現在開発中</p>
+    <p>工夫：関門に由来するキャラクターやストーリー作り<br>
+      実績：九州アプリチャレンジ・キャラバン 企業賞(ハウインターナショナル様)<br>
+    </p>
+
+    <hr>
+
+    <h2>Others</h2>
+    <p>・「Kasasasu」：三週間のインターンシップで開発した天気情報Webアプリ <br>
+      ・procon28：競技部門．敗者復活から決勝戦まで進出．総合10位．<br>
+      ・高専祭HP:<a
+        href="https://www.kct.ac.jp/katudou/student/kctfes2016/index.html"
+        target="_blank">北九州高専 第50回高専祭 RIOT</a>
+    </p>
+
   </div>
 </template>
 
